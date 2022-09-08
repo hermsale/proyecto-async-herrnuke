8b6675d7fa5be875e73ad6f78dafb53e53e75ec6
Alejandro Hugo Herms <hermsale@gmail.com>
2022-09-07 22:35:35 -0300

    se logro mejoras en la interfaz del canal de videos de Ultimos Lanzamientos. Logrando un acceso a cada video. Se agrego la Descripcion al pie del titulo de cada video

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,6 +7,7 @@ const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCxfYb0xYwM9-An
 
 const content = null || document.getElementById('content');
 const body = document.querySelector('body');
+const btnVideoId = document.querySelector('#btn-videoId');
 
 const options = {
 	method: 'GET',
@@ -15,6 +16,8 @@ const options = {
 		'X-RapidAPI-Key': '1f8a6aaa2bmsh0d494ed21e51951p1ee721jsne008e3968bd8'
 	}
 };
+
+
 
 // fetch('', options)
 // 	.then(response => response.json()) // obtenemos la primera respuesta y la transformamos a JSON 
@@ -26,37 +29,39 @@ async function fetchData(urlApi){
     const data = await response.json();
     return data;
 }
-
+// target="_blank"
 // creamos una funcion async anonima, para la carga de la funcion fetchData
 (async () => {
     try{
 
-        const videos = await fetchData(API); // cargamos el canal de videos 
+        const channelVideos = await fetchData(API); // cargamos el canal de videos 
         console.log('consulta');
         // generamos un template en base a la logica del html, para cargarle lo que recibimos de la API 
         // hacemos un map de videos.item para regresar un nuevo arreglo, pero con la transformacion del template de cada uno de los elementos del array que obtengo
         let view = `
-            ${videos.items.map(video => `
-            <div class="group relative">
-                <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            ${channelVideos.items.map(video => `
+            <div class="group relative" >
+                <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none" id="btn-videoId"> 
                     
                     <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                    
                     
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm  text-neutral-300">
-                        <a href="https://www.youtube.com/channel/${video.snippet.channelId}" target="_blank">
+                        <a href="https://www.youtube.com/watch?v=${video.id.videoId}" onclick=console.log('entra a la lista de videos en YT') target="_blank">
                         <span aria-hidden="true" class="absolute inset-0">
                         </span>
                         </a>
-                        ${video.snippet.title}
-                        
+                        ${video.snippet.title}   <br>  
+                        ${video.snippet.description}
                     </h3>
                 </div>
             </div>
             `).slice(0,3).join('')} 
         `;
         content.innerHTML = view; // agregamos a content lo que haya en view
+        // console.log(channelVideos); // utilizado para visualizar el objeto 
     } 
     catch(error){
         console.error(error);
@@ -71,3 +76,5 @@ async function fetchData(urlApi){
         body.innerHTML = viewError;
     }
 })();
+
+
